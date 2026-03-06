e9faf147c73a784c29a1228ebf0af49b9fa34096
Adam Matthews <>
2026-03-06 14:29:25 +0000

    loop printing hello world every 5 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use tokio::time::interval;
+
+#[tokio::main]
+async fn main() {
+    let mut ticker = interval(Duration::from_secs(5));
+    loop {
+        ticker.tick().await;
+        println!("Hello, world!");
+    }
 }
